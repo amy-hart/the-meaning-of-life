977d5d3b609d0868a88b6cf72cd58cacb6637dc1
amy-hart <amy.hart@autotrader.co.uk>
2022-05-10 09:00:36 +0100

    Initial commit

---
+++ src/main/java/com/amyhart/meaningoflife/MeaningOfLifeApplication.java
@@ -0,0 +1,13 @@
+package com.amyhart.meaningoflife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeaningOfLifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeaningOfLifeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amyhart/meaningoflife/MeaningOfLifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amyhart.meaningoflife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeaningOfLifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
